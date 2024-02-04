1cbca2356d6db5037a712226a39a2f14f1c89e87
ARidwanW <alwandia.wadiska@m3.formulatrix.com>
2024-02-04 21:28:50 +0700

    Migrations database identity user

--- basic-webapi/WebAPI.Persistence/Migrations/20240204142738_UsingIdentityUser.Designer.cs
+++ basic-webapi/WebAPI.Persistence/Migrations/20240204142738_UsingIdentityUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPI.Persistence;
 
@@ -10,9 +11,11 @@ using WebAPI.Persistence;
 namespace WebAPI.Persistence.Migrations
 {
     [DbContext(typeof(MyDatabase))]
-    partial class MyDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20240204142738_UsingIdentityUser")]
+    partial class UsingIdentityUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
