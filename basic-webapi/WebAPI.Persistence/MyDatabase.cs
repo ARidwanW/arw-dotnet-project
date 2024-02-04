334b6a4dc0e06b78c81082aaf2f83e007da5011c
ARidwanW <alwandia.wadiska@m3.formulatrix.com>
2024-02-04 17:55:10 +0700

    Add inherit class DbContext

---
+++ basic-webapi/WebAPI.Persistence/MyDatabase.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Domain;
+
+namespace WebAPI.Persistence;
+
+public class MyDatabase : DbContext
+{
+    public MyDatabase()
+    { }
+    public MyDatabase(DbContextOptions<MyDatabase> options) : base(options)
+    { }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Category>(cat =>
+        {
+            cat.HasKey(c => c.CategoryId);
+            cat.Property(c => c.CategoryName).IsRequired(true);
+            cat.Property(c => c.Description).IsRequired(false);
+            cat.HasMany(c => c.Products).WithOne(p => p.Category);
+        });
+        modelBuilder.Entity<Product>(prod =>
+        {
+            prod.HasKey(p => p.ProductId);
+            prod.Property(p => p.ProductName).IsRequired(true);
+            prod.Property(p => p.Description).IsRequired(false);
+        });
+    }
+}
