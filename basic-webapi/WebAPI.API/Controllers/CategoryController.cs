fe0db7ab2c674c3671f745543309544547d61d91
ARidwanW <alwandia.wadiska@m3.formulatrix.com>
2024-02-04 18:51:31 +0700

    Add basic CRUD action for category controller

--- basic-webapi/WebAPI.API/Controllers/CategoryController.cs
+++ basic-webapi/WebAPI.API/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WebAPI.Domain;
 using WebAPI.Persistence;
 
 namespace WebAPI.API.Controllers;
@@ -12,6 +13,7 @@ public class CategoryController : BaseApiController
         _myDatabase = myDatabase;
     }
     #region Action
+    [HttpGet]   //* api/category
     public async Task<IActionResult> GetCategories()
     {
         var category = await _myDatabase.Categories.ToListAsync();
@@ -19,6 +21,42 @@ public class CategoryController : BaseApiController
         {
             return NotFound();
         }
+        return Ok(category);
+    }
+    [HttpGet("{id}")]   //* api/category/{id}
+    public async Task<IActionResult> GetCategory([FromRoute]Guid id)
+    {
+        Category? category = await _myDatabase.Categories.FindAsync(id);
+        if(category is null)
+        {
+            return NotFound();
+        }
+        return Ok(category);
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory([FromBody]Category category)
+    {
+        _myDatabase.Categories.Add(category);
+        await _myDatabase.SaveChangesAsync();
+        return Ok(category);
+    }
+    [HttpPut]
+    public async Task<IActionResult> UpdateCategory([FromBody]Category category)
+    {
+        _myDatabase.Categories.Update(category);
+        await _myDatabase.SaveChangesAsync();
+        return Ok(category);
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCategory([FromRoute]Guid id)
+    {
+        Category? category = await _myDatabase.Categories.FindAsync(id);
+        if(category is null)
+        {
+            return NotFound();
+        }
+        _myDatabase.Categories.Remove(category);
+        await _myDatabase.SaveChangesAsync();
         return Ok(category);
     }
     #endregion
