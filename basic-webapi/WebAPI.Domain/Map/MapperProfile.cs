aa7dd24d25564e2f0ee5f718161a6cbcc276107b
ARidwanW <alwandia.wadiska@m3.formulatrix.com>
2024-02-04 19:23:35 +0700

    Add mapper profile for category DTOs

--- basic-webapi/WebAPI.Domain/Map/MapperProfile.cs
+++ basic-webapi/WebAPI.Domain/Map/MapperProfile.cs
@@ -1,5 +1,14 @@
+using AutoMapper;
+using WebAPI.Domain.DTOs.Request;
+using WebAPI.Domain.DTOs.Response;
+
 namespace WebAPI.Domain.Map;
 
-public class MapperProfile
+public class MapperProfile : Profile
 {
+    public MapperProfile()
+    {
+        CreateMap<Category,CategoryResponse>();
+        CreateMap<CategoryRequest, Category>();
+    }
 }
