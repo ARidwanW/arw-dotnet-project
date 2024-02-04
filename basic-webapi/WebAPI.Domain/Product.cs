1600a4cc2f87923730c228a17fbc1b586aa54132
ARidwanW <alwandia.wadiska@m3.formulatrix.com>
2024-02-04 19:02:42 +0700

    Remove unnecessary MyProperty

--- basic-webapi/WebAPI.Domain/Product.cs
+++ basic-webapi/WebAPI.Domain/Product.cs
@@ -5,7 +5,6 @@ public class Product
     public Guid ProductId { get; set; }
     public string ProductName { get; set; } = null!;
     public string? Description { get; set; }
-    public int MyProperty { get; set; }
     public int Stock { get; set; }
     public int Price { get; set; }
     public Guid CategoryId { get; set; }
