ec501d657a85b119ab25d5cc2af27e8ec5872ce0
ARidwanW <alwandia.wadiska@m3.formulatrix.com>
2024-02-04 22:48:54 +0700

    Authenticate the JWT

--- basic-webapi/WebAPI.API/Program.cs
+++ basic-webapi/WebAPI.API/Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using WebAPI.API.Services;
 using WebAPI.Domain;
 using WebAPI.Domain.Map;
@@ -15,15 +18,30 @@ builder.Services.AddIdentityCore<AppUser>(opt =>
 })
 .AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<MyDatabase>();    //* package: Microsoft.AspNetCore.Identity.EntityFrameworkCore
-builder.Services.AddAuthentication();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(opt => opt.TokenValidationParameters = new TokenValidationParameters()
+{
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+    ValidAudience = builder.Configuration["Jew:Audience"],
+    IssuerSigningKey = new SymmetricSecurityKey
+    (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+});
 
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(typeof(MapperProfile));  //* package: AutoMapper.Extensions.Microsoft.DependencyInjection
+
 builder.Services.AddDbContext<MyDatabase>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddScoped<TokenServices>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -38,6 +56,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- basic-webapi/WebAPI.API/Services/TokenServices.cs
+++ basic-webapi/WebAPI.API/Services/TokenServices.cs
@@ -9,6 +9,11 @@ namespace WebAPI.API.Services;
 
 public class TokenServices
 {
+    private IConfiguration _configuration;
+    public TokenServices(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public string CreateToken(AppUser user)
     {
         var claims = new List<Claim>()
@@ -17,15 +22,20 @@ public class TokenServices
             new Claim(ClaimTypes.NameIdentifier, user.Id),
             new Claim(ClaimTypes.Email, user.Email)
         };
+        // SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
+        // ("ThisIsSup3r53cR3TThisIsSup3r53cR3TThisIsSup3r53cR3TThisIsSup3r53cR3T"));
+        // //* At least 12 characters to secret key
         SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes
-        ("ThisIsSup3r53cR3TThisIsSup3r53cR3TThisIsSup3r53cR3TThisIsSup3r53cR3T"));
+        (_configuration["Jwt:Key"]));
         //* At least 12 characters to secret key
         SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
         SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor()
         {
             Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddDays(7),
-            SigningCredentials = creds
+            SigningCredentials = creds,
+            Issuer = _configuration["Jwt:Issuer"],
+            Audience = _configuration["Jwt:Audience"]
         };
         JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
         SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
