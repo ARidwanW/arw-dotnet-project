45a09086532ef2d14b8c144cd34e9dcdcaa2d7d2
ARidwanW <alwandia.wadiska@m3.formulatrix.com>
2024-02-04 19:17:42 +0700

    Add DTO response for category

---
+++ basic-webapi/WebAPI.Domain/DTOs/Response/CategoryResponse.cs
@@ -0,0 +1,8 @@
+namespace WebAPI.Domain.DTOs.Response;
+
+public class CategoryResponse
+{
+    public Guid CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string Description { get; set; }
+}
