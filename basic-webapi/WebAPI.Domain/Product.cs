69cb1887fc72331b630c3bba8fd53675960f836b
ARidwanW <alwandia.wadiska@m3.formulatrix.com>
2024-02-04 18:40:59 +0700

    Fix product.categoryid from int to guid

--- basic-webapi/WebAPI.Domain/Product.cs
+++ basic-webapi/WebAPI.Domain/Product.cs
@@ -8,6 +8,6 @@ public class Product
     public int MyProperty { get; set; }
     public int Stock { get; set; }
     public int Price { get; set; }
-    public int CategoryId { get; set; }
+    public Guid CategoryId { get; set; }
     public Category Category { get; set; } = null!;
 }
